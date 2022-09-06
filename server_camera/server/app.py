091b5d2c8cc31555a8f85dc62583a9815aec6e47
Nicolas44Hernandez <n44hernandezp@gmail.com>
2022-09-06 16:50:36 +0200

    wifi connection status service

--- server_camera/server/app.py
+++ server_camera/server/app.py
@@ -7,6 +7,7 @@ import yaml
 from flask import Flask
 from .managers.thread_manager import thread_manager_service
 from .managers.doorbell_manager import doorbell_manager_service
+from .managers.wifi_connection_manager import wifi_connection_manager_service
 from .rest_api.thread_setup import bp as thread_controler_bp
 from .extension import api
 from .common import ServerCameraException, handle_server_camera_exception
@@ -74,6 +75,8 @@ def register_extensions(app: Flask):
     thread_manager_service.init_app(app=app)
     # Doorbell manager extension
     doorbell_manager_service.init_app(app=app)
+    # Wifi connection manager extention
+    wifi_connection_manager_service.init_app(app=app)
 
 
 def register_blueprints(app: Flask):
