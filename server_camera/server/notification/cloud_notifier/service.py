8b031cd626e7720d0db903086167d7406e6211c8
Nicolas44Hernandez <n44hernandezp@gmail.com>
2022-09-30 22:24:03 +0200

    server_tv update

--- server_camera/server/notification/cloud_notifier/service.py
+++ server_camera/server/notification/cloud_notifier/service.py
@@ -12,7 +12,7 @@ class CloudNotifier:
     """Manager for CloudNotifier"""
 
     rpi_cloud_ip: str = None
-    rpi_cloud_port: str
+    rpi_cloud_ports: str
     rpi_cloud_video_stream_path: str
 
     def __init__(self, app: Flask = None) -> None:
@@ -26,7 +26,7 @@ class CloudNotifier:
 
             # retrieve RPI box ip
             self.rpi_cloud_ip = ip_discovery_service.get_ip_addr(mac=app.config["RPI_CLOUD_MAC"])
-            self.rpi_cloud_port = app.config["RPI_CLOUD_PORT"]
+            self.rpi_cloud_ports = app.config["RPI_CLOUD_PORTS"]
             self.rpi_cloud_video_stream_path = app.config["RPI_CLOUD_EVENT_PATH"]
 
     def notify_video_stream_ready(self, stream_ready: bool, trigger: str = None):
@@ -35,18 +35,19 @@ class CloudNotifier:
         logger.info(f"Posting HTTP to notify video stream status {stream_ready} to RPI cloud")
 
         # Post video stream status to rpi cloud
-        post_url = (
-            f"http://{self.rpi_cloud_ip}:{self.rpi_cloud_port}/{self.rpi_cloud_video_stream_path}"
-        )
-        try:
-            headers = {"Content-Type": "application/x-www-form-urlencoded"}
-            data = {"status": stream_ready, "trigger": trigger}
-            rpi_cloud_response = requests.post(post_url, data=(data), headers=headers)
-            logger.info(f"RPI cloud server response: {rpi_cloud_response.text}")
-        except (ConnectionError, InvalidURL):
-            logger.error(
-                f"Error when posting wifi info to rpi cloud, check if rpi cloud server is running"
+        for port in self.rpi_cloud_ports:
+            post_url = (
+                f"http://{self.rpi_cloud_ip}:{port}/{self.rpi_cloud_video_stream_path}"
             )
+            try:
+                headers = {"Content-Type": "application/x-www-form-urlencoded"}
+                data = {"status": stream_ready, "trigger": trigger}
+                rpi_cloud_response = requests.post(post_url, data=(data), headers=headers)
+                logger.info(f"RPI cloud server response: {rpi_cloud_response.text}")
+            except (ConnectionError, InvalidURL):
+                logger.error(
+                    f"Error when posting wifi info to rpi cloud, check if rpi cloud server is running"
+                )
 
 
 cloud_notifier_service: CloudNotifier = CloudNotifier()
