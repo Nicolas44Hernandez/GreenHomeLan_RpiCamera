20f5180617fce2f2ff002a74a15a9aca85521a0c
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-11-14 11:06:22 +0100

    WIP: thread dongle update

---
+++ server_camera/server/interfaces/thread_dongle_interface/service.py
@@ -0,0 +1,68 @@
+"""
+Thread dongle interface service
+"""
+import time
+import logging
+import threading
+import serial
+from server.common import ServerCameraException, ErrorCode
+from queue import Queue, Empty
+
+
+logger = logging.getLogger(__name__)
+
+class ThreadClientDongle(threading.Thread):
+    """Service class for thread client management"""
+
+    thread_serial_port: str
+    running: bool
+    msg_callback: callable
+    keep_alive_callback: callable
+    serial_interface: serial.Serial
+
+    def __init__(self, thread_serial_port: str):
+        self.thread_serial_port = thread_serial_port
+        self.msg_callback = None
+        self.keep_alive_callback = None
+        # self.running = False
+
+        # Run Thread interface dedicated thread
+        # self.running = True
+        # Call Super constructor
+        self.serial_interface = serial.Serial(
+            self.thread_serial_port, 115200, stopbits=serial.STOPBITS_ONE
+        )
+        super(ThreadClientDongle, self).__init__(name="ThreadClientDongleThread")
+        self.setDaemon(True)
+
+    def enqueue_output(self, out, queue):
+        for line in iter(out.readline, b""):
+            queue.put(line)
+        out.close()
+
+    def run_dedicated_thread(self):
+        """Run Thread loop in dedicated if network is setted up"""
+        logger.info(f"Running Thread loop in dedicated thread")
+        try:
+            self.start()
+        except Exception as e:
+            logger.error(e)
+
+    def run(self):
+        """Run thread"""
+
+        while True:
+            if self.serial_interface.inWaiting() > 0:
+                received_data = self.serial_interface.read(
+                    self.serial_interface.inWaiting()
+                )
+                print("Received data: ", received_data.decode("utf-8"))
+            time.sleep(0.1)
+
+    def set_msg_reception_callback(self, callback: callable):
+        """Set Thread message reception callback"""
+        self.msg_callback = callback
+
+    def set_keep_alive_reception_callback(self, callback: callable):
+        """Set Thread keep_alive reception callback"""
+        self.keep_alive_callback = callback
--- server_camera/server/managers/thread_manager/service.py
+++ server_camera/server/managers/thread_manager/service.py
@@ -1,23 +1,19 @@
 import logging
-from datetime import timedelta
 from timeloop import Timeloop
 from flask import Flask
-from server.interfaces.thread_interface import ThreadInterface
+from server.interfaces.thread_dongle_interface import ThreadDongleInterface
 
 logger = logging.getLogger(__name__)
 
 thread_keep_alive_timeloop = Timeloop()
 
+
 class ThreadManager:
     """Manager for thread interface"""
 
-    thread_interface: ThreadInterface
+    thread_dongle_interface: ThreadDongleInterface
     serial_interface: str
     serial_speed: int
-    thread_udp_port: int
-    ipv6_mesh: str
-    dataset_key: str
-    device_id: str
 
     def __init__(self, app: Flask = None) -> None:
         if app is not None:
@@ -30,66 +26,24 @@ class ThreadManager:
             self.thread_interface = None
             self.serial_interface = app.config["THREAD_SERIAL_INTERFACE"]
             self.serial_speed = app.config["THREAD_SERIAL_SPEED"]
-            self.thread_udp_port = app.config["THREAD_UDP_PORT"]
-            self.ipv6_mesh = app.config["THREAD_IPV6_MESH"]
-            self.dataset_key = app.config["THREAD_DATSET_KEY"]
-            self.device_id = app.config["THREAD_DEVICE_ID"]
-
 
             # Create Thread interface
-            self.thread_interface = ThreadInterface(
-                serial_interface=self.serial_interface,
-                serial_speed=self.serial_speed,
-                thread_udp_port=self.thread_udp_port,
+            self.thread_interface = ThreadDongleInterface(
+                thread_serial_port=self.serial_interface
             )
-
-            logger.info(f"Join Thread network")
-
-            if not self.thread_interface.setup_thread_node(
-                ipv6_mesh=self.ipv6_mesh,
-                dataset_key=self.dataset_key,
-            ):
-                logger.error(f"Error in thread node setup")
-                return
-
-            # Schedule keep alive messages
-            self.schedule_thread_keep_alive_message_send()
-
-
-    def send_keep_alive_message(self):
-        """Send keep alive message via thread"""
-        message = f"ka_{self.device_id}"
-        logger.info("sending Thread keep alive msg")
-        self.send_thread_message_to_border_router(message)
-
-    def schedule_thread_keep_alive_message_send(self):
-        """Schedule KA thread message"""
-
-        @thread_keep_alive_timeloop.job(interval=timedelta(seconds=20))
-        def send_keep_alive():
-            self.send_keep_alive_message()
-
-        thread_keep_alive_timeloop.start(block=False)
-
 
     def send_thread_message_to_border_router(self, message: str):
         """Send message to border router"""
+        pass
 
-        if self.thread_interface.running:
-            self.thread_interface.send_message_to_border_router(message)
-        else:
-            logger.error(
-                "Thread network not configured or not running, wating for network setup message"
-            )
-            logger.error("Message not published")
+        # if self.thread_interface.running:
+        #     self.thread_interface.send_message_to_border_router(message)
+        # else:
+        #     logger.error(
+        #         "Thread network not configured or not running, wating for network setup message"
+        #     )
+        #     logger.error("Message not published")
 
-    def get_thread_network_setup(self):
-        """Return thread network setup"""
-        network_setup = {
-            "ipv6_mesh": self.ipv6_mesh,
-            "dataset_key": self.dataset_key,
-        }
-        return network_setup
 
 thread_manager_service: ThreadManager = ThreadManager()
 """ Thread manager service singleton"""
