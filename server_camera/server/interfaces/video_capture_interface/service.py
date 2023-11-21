7f8e3a1c27fe25cbb516d83b593fd5543b017698
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-11-21 13:13:29 +0100

    WIP: requirements update + service update

--- server_camera/server/interfaces/video_capture_interface/service.py
+++ server_camera/server/interfaces/video_capture_interface/service.py
@@ -3,7 +3,9 @@ Video capture interface service
 """
 import logging
 import threading
-import cv2
+from picamera2 import Picamera2
+import io
+import time
 from datetime import datetime, timedelta
 from flask import Response
 
@@ -16,10 +18,14 @@ class VideoCaptureInterface(threading.Thread):
     """Service class for video manager interface management"""
 
     fps: int
-    capture: cv2.VideoCapture
+    picam: Picamera2
 
     def __init__(self):
-        self.capture = cv2.VideoCapture(0)
+        self.picam = Picamera2()
+        camera_config = self.picam.create_preview_configuration()
+        self.picam.configure(camera_config)
+        self.picam.start()
+        time.sleep(2)
         logger.info("Video manager interface started")
 
     def gen_frames(self, duration_in_secs: int):
@@ -27,26 +33,34 @@ class VideoCaptureInterface(threading.Thread):
         estimated_end = datetime.now() + timedelta(seconds=duration_in_secs)
         now = datetime.now()
         while now < estimated_end:
-            success, frame = self.capture.read()  # read the camera frame
-            if not success:
-                logger.error("Error in video capture")
-                continue
-            else:
-                frame = cv2.resize(
-                    frame, None, fx=PICTURE_RATIO, fy=PICTURE_RATIO, interpolation=cv2.INTER_AREA
-                )
-                ret, buffer = cv2.imencode(".jpg", frame)
-                if not ret:
-                    logger.error("Error in image encode")
-                    continue
-                frame = buffer.tobytes()
-                yield (
-                    b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
-                )  # concat frame one by one and show result
+            # TODO:  Try exception
+            # frame = self.picam.capture_array() # read the camera frame
+            stream = io.BytesIO()
+            self.picam.capture_file(stream, format="jpeg")  # this is the laggy bit
+            stream.seek(0)  # goes to start of buffer
+            frame = stream.read()
+            stream.truncate()  # cleans up buffer memory
+
+            # frame = cv2.resize(
+            #     frame,
+            #     None,
+            #     fx=PICTURE_RATIO,
+            #     fy=PICTURE_RATIO,
+            #     interpolation=cv2.INTER_AREA,
+            # )
+            # ret, buffer = cv2.imencode(".jpg", frame)
+            # if not ret:
+            #     logger.error("Error in image encode")
+            #     continue
+            # frame = buffer.tobytes()
+            yield (
+                b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+            )  # concat frame one by one and show result
             now = datetime.now()
         logger.info("Stream finished")
 
     def get_video_stream(self, duration_in_secs: int):
         return Response(
-            self.gen_frames(duration_in_secs), mimetype="multipart/x-mixed-replace; boundary=frame"
+            self.gen_frames(duration_in_secs),
+            mimetype="multipart/x-mixed-replace; boundary=frame",
         )
